54fe1bd771d579d62c37afd11a860db7e36fc314
arefe_hamidi <arefehamidi.guilan@gmail.com>
2023-09-02 17:41:12 +0330

    create store files

---
+++ src/store/cart-slice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {},
+  reducers: {},
+});
+export const cartActions = cartSlice.actions;
+export default cartSlice.reducer;
