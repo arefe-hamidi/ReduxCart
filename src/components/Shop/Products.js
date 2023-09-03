4a3cd6f0d8a3691fc3f4857371b39f08b3d7ae50
arefe_hamidi <arefehamidi.guilan@gmail.com>
2023-09-03 14:04:27 +0330

    use store for control cards

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+const DUMMY_PRODUCTS = [
+  {
+    id: 1,
+    price: 6,
+    title: "my first book",
+    description: "This is a first product - amazing!",
+  },
+  {
+    id: 2,
+    price: 6,
+    title: "my second book",
+    description: "This is a first product - amazing!",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
